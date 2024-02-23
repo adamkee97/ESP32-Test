3d820f614164ea77311aad05f08b62dfb44f0625
Adam Kee 紀韋延 <adamkee97@gmail.com>
2024-02-23 22:15:01 +0800

    Initial branch commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
+#include "ELECHOUSE_CC1101_SRC_DRV.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+  ELECHOUSE_cc1101.Init();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // placeholder
 }
